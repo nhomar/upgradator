3727adb8ac22e37e7a483d8e5c56962f854f910e
Nhomar - Vauxoo <nhomar@gmail.com>
2014-09-04 04:22:21 -0430

    [IMP] upgrade_code: Better documentation with correct approach following sphinx standards.

--- upgradator/upgrade_code.py
+++ upgradator/upgrade_code.py
@@ -1,67 +1,86 @@
 #!/usr/bin/env python
-#-*- coding:utf-8 -*-
-import os
-#######################
-# constants declaration
-#######################
+# -*- coding:utf-8 -*-
 
+import os
+
+"""
+A little lib where all particularities from one version to another will be
+taken into account basically here you will find a set of "helper" methods to be
+used as tools to include the correct elements on python files.
+"""
 
 class migrateNextVersion():
+    '''Create to prepare fields to migrate in V7 openerp Change the import and
+    inherits method for run in the V7 Object to set the enviroment elements to
+    work with.
 
-    '''
-    Create to prepare fields to migrate in V7 odoo
-    Change the import and inherits method for run in the V7
+    :param list l_files: List with path for any file with python code to migrate
+    :param str version: Version to migrate to.
+    :returns: object
+
     '''
 
     def __init__(self, l_files=None, version='7.0'):
-        '''
-        Maker to add list files and call for any method in this class
-        @param l_files  List whit path for any file with python code to migrate
-
-        '''
         self.version = '7.0'
         self.l_files = type(l_files) is list and l_files or l_files and \
             [l_files]
 
     def change_import_method(self, line):
-        '''
-        Change the method to import the necessary libraries for odoo modules
-        @param line String with method import line
+        '''namespace openerp.* must be respected due to new framework way to work
+        this method ensure the line which import osv respect those name space.
+
+        >>> from upgrade_code import migrateNextVersion
+
+        Set the python files in a list walking in the folder.
+
+        >>> mnv = migrateNextVersion(['file1.py', 'file2.py'])
+
+        If the import is not an Openerp ones they are simply ignored
+
+        >>> mnv.change_import_method('import something')
+        'import something'
+
+        If it doesn't respect the namespace it is changed
+
+        >>> mnv.change_import_method('from osv import fields, osv\\n')
+        'from openerp.osv import osv, fields\\n'
+
+        :param str line: with method import line
 
         '''
         text = line
         if line.find('from osv', 0, 8) >= 0:
 
             if 'fields' in line and line.find('osv', 8) < 0:
-                text = 'from odoo.osv import fields, osv\n'
+                text = 'from openerp.osv import fields, osv\n'
 
             elif line.find('osv', 8) > 0 and not 'fields' in line:
                 text = ''
 
             elif 'fields' in line and line.find('osv', 8) > 0 and 'orm' \
                     not in line:
-                text = 'from odoo.osv import osv, fields\n'
+                text = 'from openerp.osv import osv, fields\n'
 
             elif 'fields' in line and line.find('osv', 8) > 0 and \
                  'orm' in line:
-                text = 'from odoo.osv import osv, fields, orm\n'
+                text = 'from openerp.osv import osv, fields, orm\n'
 
         elif line.find('import netsvc', 0, 13) >= 0:
-            text = 'import odoo.netsvc as netsvc\n'
+            text = 'import openerp.netsvc as netsvc\n'
 
         elif line.find('from tools.', 0, 13) >= 0:
-            text = 'from odoo.tools%s\n' % line[10:]
+            text = 'from openerp.tools%s\n' % line[10:]
 
         elif line.find('import tools', 0, 13) >= 0:
-            text = 'import odoo.tools as tools\n'
+            text = 'import openerp.tools as tools\n'
 
         return text
 
     def change_inherits_class(self, line):
-        '''
-        Now we have a new method to inherit class from odoo this method
-        change it
-        @param line String with class definition line
+        '''Now we have a new method to inherit class from openerp this method
+        change it and set new elements to classes
+
+        :param str line: class definition
         '''
 
         text = line
@@ -75,14 +94,12 @@ class migrateNextVersion():
         return text
 
     def main(self, files, change_type='a'):
-        '''
-        Process to read lines from files and change it if is necessary calling
+        '''Process to read lines from files and change it if is necessary calling
         corresponding method
-        @param files List or string with path for all .py files
-        @param change_type Change type to script:
-                           a: All change class definition and import method
-                           cl: Change only class definition
-                           im: Change only import method
+
+        :param list files: List or string with path for all .py files
+        :param str change_type: Change type to script 'a' All change class definition and import method 'cl' Change only class definition 'im' Change only import method
+        :returns boolean
         '''
 
         files = self.l_files or type(files) is list and files or [files]
@@ -113,3 +130,7 @@ class migrateNextVersion():
             copy_f.close()
 
         return True
+
+if __name__=='__main__':
+    import doctest
+    doctest.testmod()
